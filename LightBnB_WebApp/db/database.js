1b22ee8c02f2c4ab8465247674b5c9cd1bd70ba2
Kim O'Dell <kimbo.odell@gmail.com>
2024-04-16 21:10:24 -0400

    Refactor addProperty function to use SQL query

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -167,7 +167,7 @@ const getAllProperties = (options, limit = 10) => {
   //if maximum cost per night is added as a peramter 
   if (options.maximum_price_per_night) {
     queryParams.push(parseInt(options.maximum_price_per_night, 10) * 100);
-    queryString += `${queryParams.length > 1  ? ' AND' : 'WHERE'} cost_per_night <= $${queryParams.length} `;
+    queryString += `${queryParams.length > 1 ? ' AND' : 'WHERE'} cost_per_night <= $${queryParams.length} `;
   }
 
   queryString += `
@@ -206,10 +206,78 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  // const propertyId = Object.keys(properties).length + 1;
+  // property.id = propertyId;
+  // properties[propertyId] = property;
+  // return Promise.resolve(property);
+
+  const {
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  } = property;
+
+  const queryParams = [
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  ];
+
+  const queryString = `
+    INSERT INTO properties (
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      street,
+      city,
+      province,
+      post_code,
+      country,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms
+    ) VALUES (
+      $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14
+    ) RETURNING *;
+  `;
+
+  return pool
+    .query(queryString, queryParams)
+    .then((result) => {
+      //return newly added user property from database
+      if (result.rows.length) {
+        return result.rows[0];
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
 };
 
 module.exports = {
